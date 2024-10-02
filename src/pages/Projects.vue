1e73133b93abed4a94680565a4442e8732752741
Samuele <samueletasciotti@icloud.com>
2024-10-02 17:34:18 +0200

    fix: print tags and categoryes

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,30 +1,30 @@
 <script>
-import axios from 'axios'; // Import libreria axios;
-import { store } from '../store/store';
+import axios from "axios"; // Import libreria axios;
+import { store } from "../store/store";
 
 export default {
   name: "Projects",
-  data(){
-    return{
-        store,
-        projects: [], // Creo un array vuoto dove salverò i progetti;
-        tags: [] // Creo un array vuoto dove salverò tutti i TAG;
-    }
+  data() {
+    return {
+      store,
+      projects: [], // Creo un array vuoto dove salverò i progetti;
+      tags: [], // Creo un array vuoto dove salverò tutti i TAG;
+    };
   },
-  mounted(){
+  mounted() {
     this.projectsCall();
     // this.tagsCall();
   },
-  methods:{
+  methods: {
     projectsCall() {
-        axios
-        .get(store.apiURL + 'projects') // Chiamata API dei Progetti;
-        .then(response => {
-            this.projects = response.data.data;
+      axios
+        .get(store.apiURL + "projects") // Chiamata API dei Progetti;
+        .then((response) => {
+          this.projects = response.data.data;
         })
-        .catch(error => {
+        .catch((error) => {
           // Gestione degli errori
-          console.error('Errore durante il recupero dei dati', error); //Messaggio di errore in caso di non riuscita;x\
+          console.error("Errore durante il recupero dei dati", error); //Messaggio di errore in caso di non riuscita;x\
         });
     },
     // tagsCall() {
@@ -38,45 +38,55 @@ export default {
     //     console.error('Errore durante il recupero dei dati', error); //Messaggio di errore in caso di non riuscita;x\
     //   });
     // }
-  }
+  },
 };
 </script>
 
 <template>
-<div id="view" class="container my-5">
+  <div id="view" class="container my-5">
     <h1>Progetti</h1>
 
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#ID</th>
-      <th scope="col">TITLE</th>
-      <th scope="col">SLUG</th>
-      <th scope="col">DESCRIPTION</th>
-      <th scope="col">CATEGORY</th>
-      <th scope="col">TAGS</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="project in projects" :key="project.id">
-      <th>{{ project.id }}</th>
-      <td>{{ project.title }}</td>
-      <td>{{ project.slug }}</td>
-      <td>{{ project.description }}</td>
-      <td>{{ project.category.name }}</td>
-      <td v-for="tag in project.tag" :key="tag.id">{{ tag.name }}</td>
-    </tr>
-   
-   <!-- <tr v-for="tag in tags" :key="tag.id">
-    <th>{{ tag.id }}</th>
-   </tr> -->
-  </tbody>
-</table>
-</div>
+      <thead>
+        <tr>
+          <th scope="col">#ID</th>
+          <th scope="col">TITLE</th>
+          <th scope="col">SLUG</th>
+          <th scope="col">DESCRIPTION</th>
+          <th scope="col">CATEGORY</th>
+          <th scope="col">TAGS</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <th>{{ project.id }}</th>
+          <td>{{ project.title }}</td>
+          <td>{{ project.slug }}</td>
+          <td>{{ project.description }}</td>
+          <td class="category">{{ project.category.name }}</td>
+          <td>
+            <div class="tags" v-for="tag in project.tags" :key="tag.id">{{ tag.name }}</div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
-#view{
+
+/* General */
+#view {
   min-height: 100%;
+}
+
+/* TAGS | CATEGORY */
+.tags{
+  border: 2px solid black;
+  background: lightblue;
+  border-radius: 10px;
+  text-align: center;
+  padding: 5px;
+  margin: 5px;
 }
 </style>
